fb5413e6ccc9af38c2be977ae7e4705a87c5d2e4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 00:31:55 +0100

    new file:   beta_rewards/lua/betarewards/lang/en.lua => New place of lang files new file:   beta_rewards/lua/betarewards/lang/fr.lua => New place of lang files

---
+++ beta_rewards/lua/betarewards/lang/en.lua
@@ -0,0 +1,8 @@
+Beta_Rewards = Beta_Rewards or {}
+Beta_Rewards.lang = Beta_Rewards.lang or {}
+
+Beta_Rewards.lang["DBConnected"] = "Successfully connected to the DB!"
+Beta_Rewards.lang["DBError"] = "An error occured with the database:"
+Beta_Rewards.lang["DBInit"] = "Successfully initialized the database!"
+Beta_Rewards.lang["PlyReg"] = "Player successfully registered to database!"
+Beta_Rewards.lang["PlyAlreadyReg"] = "Player already on the database!"
---
+++ beta_rewards/lua/betarewards/lang/fr.lua
@@ -0,0 +1,8 @@
+Beta_Rewards = Beta_Rewards or {}
+Beta_Rewards.lang = Beta_Rewards.lang or {}
+
+Beta_Rewards.lang["DBConnected"] = "Connexion avec la base de données établie avec succès !"
+Beta_Rewards.lang["DBError"] = "Une erreur est survenue"
+Beta_Rewards.lang["DBInit"] = "Base de données initialisée !"
+Beta_Rewards.lang["PlyReg"] = "Joueur enregistré sur la base de données !"
+Beta_Rewards.lang["PlyAlreadyReg"] = "Joueur déjà enregistré dans la base de données !"
